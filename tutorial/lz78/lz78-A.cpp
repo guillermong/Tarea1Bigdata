8ac9b35581844e1213a53352bfbd5b559d45f7af
Guillermo Navarro Giglio <navarro.g.guillermo@gmail.com>
2013-10-07 21:10:31 -0300

    repair2?

--- tutorial/lz78/lz78-A.cpp
+++ tutorial/lz78/lz78-A.cpp
@@ -70,31 +70,37 @@ int lzbus(const char* pattern,size_t patternlenght, Array *prev, Array *news)
 {
 	
 	map <int  ,vector<int> > map1;
-	vector<int> lista;
 	int occ=0,d,f; 
 	unsigned char v;
 	
-	//GENERA LOS PREFIJOS Y SE GUARDAN EN MAP1
+	//GENERA LOS PREFIJOS Y SE GUARDAN EN MAP1 
+	//EJ=patron:hola map1{0,1,2,3,} donde los numeros representan las letras del patron
 	
-	for(int k=0;k<patternlenght;k++)	map1[k];		
+	for(int k=0;k<patternlenght;k++)	map1[k];
+			
 	//RECORRE TODO EL INDEX			
 					
 	for (int i = 0; i < prev->getLength(); i++) {
 		
 		//SE REVISA SI LA LETRA DEL PARRAFO ES IGUAL A LA ULTIMA LETRA DE LOS SUFIJOS
 		//cout<<i+1<<" ("<<prev->getField(i)<<","<<(unsigned char)news->getField(i)<<")"<<endl;
+		
+		//v y d son los vectores (d,v)
 		v = (unsigned char)news->getField(i);
 		d = prev->getField(i);	
+		
+		//recorre todo el patron, preguntando si la letra del vector es igual a la letra del patron
 		for(int k=patternlenght; k>0 ;k--){			
 			if(v== pattern[k-1])
-			{				
+			{			//si la letra es igual a la primera letra del patron, lo agrega a map1[0]	
 						if( k-1==0)
 						{					
 							map1[k-1].push_back(i+1);
 							break;						
 						}	
 						
-						//SOLO HACER EL FOR CUANDO DEBE HACERLO!!	
+						//si el numero que referencia en el vector se encuentra en la lista anterior lo agrega 
+						//EJ: (3,o) pregunta si 3 esta en la lista[0] () lista que presenta a la lista "h"	
 						for(int r=0; r < map1[k-2].size(); r++)
 						{
 							if( d == map1[k-2][r] )
@@ -106,6 +112,7 @@ int lzbus(const char* pattern,size_t patternlenght, Array *prev, Array *news)
 						}				
 			}			
 		}
+		//PREGUNTA SI EL NUMERO DEL VECTOR REFERENCIA A ALGUN NUMERO DE LA LISTAS "HOLA" Y SUMA UNA OCC
 		occ+=suma(map1.find(patternlenght-1)->second,prev->getField(i),prev);
 												
 	}	
